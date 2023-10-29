a5812baa420e3a45d5db0d7b66dfac3b67fcec4b
ryan suber <rsuber406@gmail.com>
2023-10-28 21:35:29 -0400

    More practice added

---
+++ 1/arrayChallenge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+string [] things = {"b123", "arc456","yurty67","8790bg","123yrts", "opys45","9087un","ayth65"};
+
+Console.WriteLine(things.Length);
+
+
+foreach(string item in things){
+    if(item.StartsWith("b") ){
+        
+        Console.WriteLine(item);
+    }
+
+
+}
---
+++ 1/arrayStuff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Common;
+
+// string[] fraudulentId = new string[3];
+
+// fraudulentId[0]= "ABC123";
+// fraudulentId[1]= "ADRT345";
+// fraudulentId[2] = "BJRU678";
+
+
+// for (int i = 0; i< fraudulentId.Length; i++){
+//     Console.WriteLine(fraudulentId[i]);
+// }
+
+string [] fraudulentId = {"AFD123", "GHFR456","JURY567"};
+
+foreach(string ids in fraudulentId){
+    Console.WriteLine(ids);
+}
+
+
+
+
