400ac6d31b761c3b263781fa9fa203cbc349e273
Ryan Suber <rsuber406@gmail.com>
2023-10-30 20:52:53 -0400

    outside of correct folder

---
+++ codeBlocks/Program.cs
@@ -0,0 +1,6 @@
+﻿bool flag = true;
+
+if(flag) Console.WriteLine($"Inside of code block: {flag}");
+
+// this is allowed but not recommended unless all of the associated if statements can be one line. Brackets are always recommended to use for readability.
+
