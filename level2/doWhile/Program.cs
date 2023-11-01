1a8ee66e998712f0f0e31c0841a68aef585c5faf
Ryan Suber <rsuber406@gmail.com>
2023-10-31 21:15:26 -0400

    terminal game in c#

---
+++ level2/doWhile/Program.cs
@@ -0,0 +1,15 @@
+﻿Random number = new Random();
+
+int current = number.Next(4 , 11);
+
+// do {
+//     current = number.Next(0, 11);
+//     Console.WriteLine(current);
+// } while(current != 7);
+
+while (current >= 3){
+     Console.WriteLine(current);
+    current = number.Next(0, 11);
+
+}
+Console.WriteLine($"Last number {current}");
---
+++ level2/terminalGame/Program.cs
@@ -0,0 +1,16 @@
+﻿int heroHealth = 100;
+int monsterHealth = 50;
+
+Random number = new Random();
+
+int damage = number.Next(1,25);
+
+
+while (heroHealth > 0 && monsterHealth > 0){
+    monsterHealth = monsterHealth - damage;
+    heroHealth = heroHealth - damage;
+    if(monsterHealth < 0) monsterHealth = 0;
+    if(heroHealth < 0) heroHealth = 0;
+    Console.WriteLine($"The monstor has {monsterHealth} remaining. Player did {damage} to the monster. Player health is {heroHealth}");
+    damage = number.Next(1, 30);
+}
