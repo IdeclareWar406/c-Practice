59134e9562f8b23cf5f269b6d7fff8f91be544f2
Ryan Suber <rsuber406@gmail.com>
2023-10-31 21:23:00 -0400

    final terminal game push

--- level2/terminalGame/Program.cs
+++ level2/terminalGame/Program.cs
@@ -1,16 +1,33 @@
-﻿int heroHealth = 100;
+﻿int heroHealth = 50;
 int monsterHealth = 50;
-
+Random dice = new Random();
 Random number = new Random();
 
 int damage = number.Next(1,25);
 
 
 while (heroHealth > 0 && monsterHealth > 0){
+    int chance = dice.Next(0 , 10);
+    Console.WriteLine($"chance {chance}");
+    if(chance > 5) continue;
+
     monsterHealth = monsterHealth - damage;
-    heroHealth = heroHealth - damage;
+    
     if(monsterHealth < 0) monsterHealth = 0;
     if(heroHealth < 0) heroHealth = 0;
-    Console.WriteLine($"The monstor has {monsterHealth} remaining. Player did {damage} to the monster. Player health is {heroHealth}");
+    if(monsterHealth <= 0){
+        Console.WriteLine($"The monstor has {monsterHealth} remaining. Player did {damage} to the monster. Player health is {heroHealth}");
+        Console.WriteLine("Player wins");
+        break;
+    }
+    else if(heroHealth <=0){
+        Console.WriteLine($"The monstor has {monsterHealth} remaining. Player did {damage} to the monster. Player health is {heroHealth}");
+        Console.WriteLine("Monster wins");
+        
+        break ;
+    }
+   
+     heroHealth = heroHealth - damage;
+    
     damage = number.Next(1, 30);
 }
